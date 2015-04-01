cfaad132cd8d4576de4493a591eeb81bb43d133c
cdively <chrisdively@gmail.com>
2015-03-31 20:01:50 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -2,11 +2,11 @@
 // Enter your mapbox map id here to reference it for the base layer
 
 var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var token = 'pk.eyJ1IjoiY2hyaXNkaXZlbHkiLCJhIjoiVkE5a2FTayJ9.Nxm6_TKyp0a1CTKuj0t7PA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
-var map = L.mapbox.map('map', mapId);
+var map = L.mapbox.map('map', chrisdively.25bdc8f3);
 
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
