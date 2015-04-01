0cee5c8c692be5bc45ee77cd5047b856f2c2a269
cdively <chrisdively@gmail.com>
2015-03-31 21:03:23 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -27,7 +27,7 @@ featureLayer.on('ready',function(){
 
 featureLayer.on ('ready',function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Hi Im the State Called' + layer.feature.properties.STATE_NAME);
+    layer.bindPopup('Hi Im the State Called ' + layer.feature.properties.STATE_NAME);
   });
 });
 
